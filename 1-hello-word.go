ccbdc5d08225873726a3626b6a810bbe2baba893
Saulo Costa <saulotarsobc@gmail.com>
2025-06-05 10:05:41 -0300

    modulo 4

--- 1-hello-word.go
+++ 1-hello-word.go
@@ -10,13 +10,32 @@
  */
 package main
 
-/* Importando o pacote fmt. Podemos importar quantos pacotes quisermos. */
-import "fmt"
+/**
+* Importando o pacote fmt. Podemos importar quantos pacotes quisermos.
+* https://pkg.go.dev/fmt
+* para importar varios pacotes podemos usar o seguinte formato:
+* import (
+*  "fmt"
+*  "math"
+*  "time"
+* )
+)
+*/
+
+// import "fmt"
+// ou
+import (
+	// "fmt"
+	// renomeando o pacote
+	// "fmt" -> "f"
+	f "fmt"
+)
 
 func main() {
 	/* Imprimindo Hello World. O println imprime uma linha. O fmt tem muitas funções:
 	* https://golang.org/pkg/fmt */
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
+	f.Println("Hello World")
 }
 
 // Para executar o código basta rodar o comando: go run 1-hello-word.go
